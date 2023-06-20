b43fbad8105403069a1caef5aa615ecc280aba9e
fanpeilun <fanpeilun@163.com>
2023-06-20 12:17:10 +0800

    init

---
+++ src/main/java/com/example/helloredhat/HelloRedhatApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloredhat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloRedhatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloRedhatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/helloredhat/HelloRedhatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloredhat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloRedhatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
